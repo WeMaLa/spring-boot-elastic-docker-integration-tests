7312f65fa1a4af67e375fff2f8231bb393f87eba
larmic <mail@larmic.de>
2018-02-21 21:14:31 +0100

    add fabric8io/docker-maven-plugin

--- src/main/kotlin/io.iconect.testing/TestingApplication.kt
+++ src/main/kotlin/io.iconect.testing/TestingApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LightBotApplication
+class TestingApplication
 
 fun main(args: Array<String>) {
-    runApplication<LightBotApplication>(*args)
+    runApplication<TestingApplication>(*args)
 }
