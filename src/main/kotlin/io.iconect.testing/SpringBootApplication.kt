be0381baaf968f4a812cfc7dc404c3b92b0fb759
larmic <mail@larmic.de>
2018-09-06 15:34:02 +0200

    Move DatabaseInitializerCommandLineRunner.kt to SpringBootApplication.kt

--- src/main/kotlin/io.iconect.testing/SpringBootApplication.kt
+++ src/main/kotlin/io.iconect.testing/SpringBootApplication.kt
@@ -2,14 +2,23 @@ package io.iconect.testing
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate
 import org.springframework.stereotype.Component
+
+@SpringBootApplication
+class TestingApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestingApplication>(*args)
+}
 
 /**
  * When not using an ElasticsearchRepository no index will be created. So this runner creates required index.
  */
 @Component
-class DatabaseInitializerCommandLineRunner @Autowired constructor(private var elasticsearchTemplate: ElasticsearchTemplate) : CommandLineRunner {
+class ElasticsearchInitializer @Autowired constructor(private var elasticsearchTemplate: ElasticsearchTemplate) : CommandLineRunner {
 
     override fun run(vararg args: String?) {
         elasticsearchTemplate.deleteIndex(User::class.java)
