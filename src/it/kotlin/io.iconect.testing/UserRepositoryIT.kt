9aee597700ae475bfe09cfc3a8648639b03a8f9c
larmic <mail@larmic.de>
2019-03-11 09:00:07 +0100

    update spring boot, remove unused dependencies, update elasticsearch to latest release

--- src/it/kotlin/io.iconect.testing/UserRepositoryIT.kt
+++ src/it/kotlin/io.iconect.testing/UserRepositoryIT.kt
@@ -1,18 +1,18 @@
 package io.iconect.testing
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
 /**
  * Demonstrates an integration spring boot test starting an elasticsearch as a docker container.
  */
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
 @ActiveProfiles("it")
 class UserRepositoryIT {
@@ -20,7 +20,7 @@ class UserRepositoryIT {
     @Autowired
     private lateinit var userRepository: UserRepository
 
-    @Before
+    @BeforeEach
     fun setUp() {
         userRepository.deleteAll()
     }
